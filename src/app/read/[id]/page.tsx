9e9b8d5584e8c6ab566c23ef057b62e58202e690
jjangyeunii <euneun0927@naver.com>
2023-08-29 14:48:03 +0900

    글읽기

--- src/app/read/[id]/page.tsx
+++ src/app/read/[id]/page.tsx
@@ -1,14 +1,18 @@
+import { Topic } from "@/model/topic";
+
 type Props = {
   params: {
     id: string;
   };
 };
 
-export default function Read({ params }: Props) {
+export default async function Read({ params }: Props) {
+  const res = await fetch(`http://localhost:9999/topics/${params.id}`);
+  const topic: Topic = await res.json();
   return (
     <section>
-      <h2>Read</h2>
-      <p>parameters: {params.id}</p>
+      <h2>{topic.title}</h2>
+      <p>{topic.body}</p>
     </section>
   );
 }
