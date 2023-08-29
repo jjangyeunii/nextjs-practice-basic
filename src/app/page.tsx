5dfc322a27e583cca6d3784909115f8bc72d5365
jjangyeunii <euneun0927@naver.com>
2023-08-29 14:06:43 +0900

    image & globalcss

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
+import hello from "@/../public/hello.jpg";
 
 export default function Home() {
   return (
     <main>
       <h2>Welcome</h2>
       <p>Hello, WEB!</p>
+      <Image src={hello} alt="welcomImage" width={250} height={200} />
     </main>
   );
 }
