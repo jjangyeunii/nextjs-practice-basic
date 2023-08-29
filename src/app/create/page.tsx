e994427c21030e1e2ba4569bddbb1bec3f22d023
jjangyeunii <euneun0927@naver.com>
2023-08-29 15:28:45 +0900

    글생성

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,3 +1,52 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { FormEvent } from "react";
+
+// form 이벤트 type 관련 참고 블로그
+// https://claritydev.net/blog/typescript-typing-form-events-in-react
+
+interface CustomElements extends HTMLFormControlsCollection {
+  title: HTMLInputElement;
+  body: HTMLTextAreaElement;
+}
+
+interface CustomForm extends HTMLFormElement {
+  readonly elements: CustomElements;
+}
+
 export default function Create() {
-  return <div>Create!</div>;
+  const router = useRouter();
+  return (
+    <form
+      onSubmit={(e: FormEvent<CustomForm>) => {
+        e.preventDefault();
+        const target = e.currentTarget.elements;
+        const title = target.title.value;
+        const body = target.body.value;
+        const options = {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ title, body }),
+        };
+        fetch(`http://localhost:9999/topics`, options)
+          .then((res) => res.json())
+          .then((result) => {
+            const lastid = result.id;
+            router.push(`/read/${lastid}`);
+          });
+      }}
+    >
+      <p>
+        <input type="text" name="title" placeholder="title" />
+      </p>
+      <p>
+        <textarea name="body" placeholder="body"></textarea>
+      </p>
+      <p>
+        <button type="submit">Create</button>
+      </p>
+    </form>
+  );
 }
